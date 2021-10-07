8d1de83199ce4194555bd66cd1a481bb3a97d375
Adeel <adeel.yaseen@invozone.com>
2021-10-07 10:32:13 +0500

    more changing

--- Practice/map.js
+++ Practice/map.js
@@ -12,3 +12,15 @@ myMap.set(key3, "I am Nabeel");
 console.log(myMap);
 
 const value1 = myMap.get(key1);
+
+console.log(value1);
+
+const arr = [10, 23, 334, 4, 23];
+
+// eslint-disable-next-line no-param-reassign
+const newArr = arr.map((val) => {
+  // eslint-disable-next-line no-param-reassign
+  val++;
+  return val;
+});
+console.log(newArr);
